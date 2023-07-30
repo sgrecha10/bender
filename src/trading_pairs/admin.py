e53dcb02ec432485ec39a9314aecdb1d01c163cf
grechnev <sgrecha10@gmail.com>
2023-07-31 01:07:45 +0300

    Added search_fields, list_filter to TradingPairAdmin

--- src/trading_pairs/admin.py
+++ src/trading_pairs/admin.py
@@ -7,10 +7,13 @@ from .models import TradingPair
 class TradingPairAdmin(admin.ModelAdmin):
     change_list_template = "admin/trading_pairs/trading_pair/change_list.html"
     list_display = (
-        'symbol', 'binance_id', 'base', 'quote', 'is_margin_trade',
+        'id', 'symbol', 'binance_id', 'base', 'quote', 'is_margin_trade',
         'is_buy_allowed', 'is_sell_allowed', 'updated', 'created',
     )
-    readonly_fields = ('id',) + list_display
+    readonly_fields = list_display
+    list_display_links = ('symbol',)
+    search_fields = ('symbol',)
+    list_filter = ('base',)
 
     def has_add_permission(self, request):
         return False
