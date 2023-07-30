c019399a81cfc431ae0f4c224c52501de4993c54
grechnev <sgrecha10@gmail.com>
2023-07-31 01:01:37 +0300

    Added has_delete_ to TradingPairAdmin, blank button

--- src/trading_pairs/admin.py
+++ src/trading_pairs/admin.py
@@ -5,8 +5,15 @@ from .models import TradingPair
 
 @admin.register(TradingPair)
 class TradingPairAdmin(admin.ModelAdmin):
+    change_list_template = "admin/trading_pairs/trading_pair/change_list.html"
     list_display = (
         'symbol', 'binance_id', 'base', 'quote', 'is_margin_trade',
         'is_buy_allowed', 'is_sell_allowed', 'updated', 'created',
     )
     readonly_fields = ('id',) + list_display
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
