673ec53913bbeb8258dc1806ac6865492452c950
grechnev <sgrecha10@gmail.com>
2023-08-24 01:02:03 +0300

    Formated data

--- src/wallet/admin.py
+++ src/wallet/admin.py
@@ -66,8 +66,8 @@ class TradeFeeAdmin(admin.ModelAdmin):
     change_list_template = "admin/wallet/trade_fee/change_list.html"
     list_display = (
         'symbol',
-        'maker_commission',
-        'taker_commission',
+        'get_maker_commission',
+        'get_taker_commission',
         'updated',
     )
     readonly_fields = list_display
@@ -98,3 +98,11 @@ class TradeFeeAdmin(admin.ModelAdmin):
         meta = self.model._meta
         url = reverse(f'admin:{meta.app_label}_{meta.model_name}_changelist')
         return HttpResponseRedirect(url)
+
+    @admin.display(description='maker_commission', ordering='maker_commission')
+    def get_maker_commission(self, obj):
+        return f'{obj.maker_commission:.2%}'
+
+    @admin.display(description='taker_commission', ordering='taker_commission')
+    def get_taker_commission(self, obj):
+        return f'{obj.taker_commission:.2%}'
