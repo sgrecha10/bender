24ec4a20a012ea5fe56eebee87d468750a17215c
grechnev <sgrecha10@gmail.com>
2023-08-25 23:22:12 +0300

    admin_utils

---
+++ src/core/utils/admin_utils.py
@@ -0,0 +1,14 @@
+from django.contrib import messages
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+
+
+def redirect_to_change_list(request, model, message=None, is_ok=True):
+    if message:
+        if is_ok:
+            messages.success(request, message)
+        else:
+            messages.warning(request, message)
+    meta = model._meta
+    url = reverse(f'admin:{meta.app_label}_{meta.model_name}_changelist')
+    return HttpResponseRedirect(url)
