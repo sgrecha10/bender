cf1d5826078637733176726565a2b883cfd33a09
grechnev <sgrecha10@gmail.com>
2023-09-06 00:14:55 +0300

    remove message_handler

--- src/streams/tasks.py
+++ src/streams/tasks.py
@@ -13,33 +13,33 @@ from streams.models import TaskManagement
 @app.task(bind=True)
 def task_diff_book_depth(self, symbol: str):
     kafka_client = KafkaProducerClient(settings.KAFKA_CLIENT, topic=symbol)
-    redis_conn = RedisClient()
-    redis_conn.flushall()
+    # redis_conn = RedisClient()
+    # redis_conn.flushall()
 
-    def handler_message(_, message):
-        json_data = json.loads(message)
+    # def handler_message(_, message):
+    #     json_data = json.loads(message)
+    #
+    #     group_prefix_bid = 'bid'
+    #     bid_list = json_data['b']
+    #     for item in bid_list:
+    #         price = item[0]
+    #         if not float(item[1]):
+    #             redis_conn.zremrangebyscore(group_prefix_bid, price, price)
+    #         else:
+    #             redis_conn.set_dom(group_prefix_bid, item)
+    #
+    #     group_prefix_ask = 'ask'
+    #     ask_list = json_data['a']
+    #     for item in ask_list:
+    #         price = item[0]
+    #         if not float(item[1]):
+    #             redis_conn.zremrangebyscore(group_prefix_ask, price, price)
+    #         else:
+    #             redis_conn.set_dom(group_prefix_ask, item)
+    #
+    #     # kafka_client.message_handler(_, message)
 
-        group_prefix_bid = 'bid'
-        bid_list = json_data['b']
-        for item in bid_list:
-            price = item[0]
-            if not float(item[1]):
-                redis_conn.zremrangebyscore(group_prefix_bid, price, price)
-            else:
-                redis_conn.set_dom(group_prefix_bid, item)
-
-        group_prefix_ask = 'ask'
-        ask_list = json_data['a']
-        for item in ask_list:
-            price = item[0]
-            if not float(item[1]):
-                redis_conn.zremrangebyscore(group_prefix_ask, price, price)
-            else:
-                redis_conn.set_dom(group_prefix_ask, item)
-
-        kafka_client.message_handler(_, message)
-
-    my_client = SpotWebsocketStreamClient(on_message=handler_message)
+    my_client = SpotWebsocketStreamClient(on_message=kafka_client.message_handler)
     my_client.diff_book_depth(symbol=symbol)
 
     is_working = True
