b902e2781e6e297ad079dda4deced871710ce357
grechnev <sgrecha10@gmail.com>
2024-09-09 00:33:59 +0300

    fix test_helper

--- src/base/test_helper.py
+++ src/base/test_helper.py
@@ -4,9 +4,10 @@ from decimal import Decimal
 
 from django.db import models
 
-from market_data.choices import IntervalCodename, INTERVAL_DURATION
+import market_data.constants as const
 from market_data.models import ExchangeInfo, Interval, Kline
 import pytz
+from django.db.models import QuerySet
 
 
 class TestHelperMixin:
@@ -21,46 +22,69 @@ class TestHelperMixin:
             **kwargs
         )
 
-    def create_interval(self,
-                        codename: str = None,
-                        value: str = None,
-                        minutes_count: int = None) -> Interval:
-        codename = codename if codename else IntervalCodename.MINUTE_1.name
+    def create_interval(self, codename: str = const.MINUTE_1, **kwargs) -> Interval:
         result, _ = Interval.objects.get_or_create(
             codename=codename,
-            defaults={
-                value: value if value else getattr(IntervalCodename, codename).value,
-                minutes_count: minutes_count if minutes_count else INTERVAL_DURATION[codename],
-            }
+            defaults={**kwargs},
         )
         return result
 
-    def create_kline(self,
+    def create_klines(self,
                      symbol: ExchangeInfo,
-                     interval: Interval = None,
+                     count: int = 1,
                      open_time: datetime = None,
-                     open_price: Decimal | float = None,
-                     high_price: Decimal | float = None,
-                     low_price: Decimal | float = None,
-                     close_price: Decimal | float = None,
-                     volume: Decimal | float = None,
-                     close_time: datetime = None,
-                     **kwargs) -> Kline:
+                     **kwargs) -> list[Kline]:
+
         now = datetime.now().replace(second=0, microsecond=0, tzinfo=pytz.UTC)
         open_time = open_time if open_time else now
-        if not close_time:
-            if not interval:
-                interval = self.create_interval(IntervalCodename.MINUTE_1.name)
-            close_time = open_time + timedelta(minutes=interval.minutes_count) - timedelta(seconds=1)
 
-        return Kline.objects.create(
-            symbol=symbol,
-            open_time=open_time,
-            open_price=open_price if open_price else 20.0,
-            high_price=high_price if high_price else 40.0,
-            low_price=low_price if low_price else 10.0,
-            close_price=close_price if close_price else 30.0,
-            volume=volume if volume else 1000.0,
-            close_time=close_time,
-            **kwargs
-        )
+        bulk_data = []
+        for i in range(count):
+            close_time = open_time + timedelta(minutes=1) - timedelta(seconds=1)
+            bulk_data.append(
+                Kline(
+                    symbol=symbol,
+                    open_time=open_time,
+                    open_price=20.0,
+                    high_price=40.0,
+                    low_price=10.0,
+                    close_price=30.0,
+                    volume=1000.0,
+                    close_time=close_time,
+                    **kwargs
+                )
+            )
+            open_time -= timedelta(minutes=1)
+
+        return Kline.objects.bulk_create(bulk_data)
+
+
+    # def create_kline(self,
+    #                  symbol: ExchangeInfo,
+    #                  interval: Interval = None,
+    #                  open_time: datetime = None,
+    #                  open_price: Decimal | float = None,
+    #                  high_price: Decimal | float = None,
+    #                  low_price: Decimal | float = None,
+    #                  close_price: Decimal | float = None,
+    #                  volume: Decimal | float = None,
+    #                  close_time: datetime = None,
+    #                  **kwargs) -> Kline:
+    #     now = datetime.now().replace(second=0, microsecond=0, tzinfo=pytz.UTC)
+    #     open_time = open_time if open_time else now
+    #     if not close_time:
+    #         if not interval:
+    #             interval = self.create_interval(IntervalCodename.MINUTE_1.name)
+    #         close_time = open_time + timedelta(minutes=interval.minutes_count) - timedelta(seconds=1)
+    #
+    #     return Kline.objects.create(
+    #         symbol=symbol,
+    #         open_time=open_time,
+    #         open_price=open_price if open_price else 20.0,
+    #         high_price=high_price if high_price else 40.0,
+    #         low_price=low_price if low_price else 10.0,
+    #         close_price=close_price if close_price else 30.0,
+    #         volume=volume if volume else 1000.0,
+    #         close_time=close_time,
+    #         **kwargs
+    #     )
--- src/indicators/tests.py
+++ src/indicators/tests.py
@@ -4,51 +4,28 @@ from market_data.models import ExchangeInfo, Interval, Kline
 from base.test_helper import TestHelperMixin
 from indicators.models import MovingAverage
 from datetime import datetime, timedelta
+import market_data.constants as const
 
 
 class IndicatorsTestCase(TestCase, TestHelperMixin):
     def setUp(self):
         self.exchange_info = self.create_exchange_info()
-        self.interval_1m = self.create_interval()
-        # self.interval_1h = self.create_interval('HOUR_1')
-        # self.create_kline = self.create_kline(symbol=self.exchange_info)
+        # self.interval_1m = self.create_interval()
+        # self.interval_1h = self.create_interval(const.HOUR_1)
 
     def test_moving_average_sma(self):
-        open_time = datetime.now().replace(second=0, microsecond=0)
-        kline_data = [
-            {  # 15
-                'high_price': 20.0,
-                'low_price': 10.0,
-                'open_time': open_time,
-            },
-            {  # 30
-                'high_price': 40.0,
-                'low_price': 20.0,
-                'open_time': open_time - timedelta(minutes=1),
-            },
-            {  # 30
-                'high_price': 50.0,
-                'low_price': 10.0,
-                'open_time': open_time - timedelta(minutes=2),
-            },
-        ]
-        for item in kline_data:
-            self.create_kline(
-                symbol=self.exchange_info,
-                open_time=item['open_time'],
-                high_price=item['high_price'],
-                low_price=item['low_price'],
-            )
-
-        kline_older = Kline.objects.all().order_by('-open_time').first()
-
-        moving_average = MovingAverage.objects.create(
-            name='some_name',
-            type=MovingAverage.Type.SMA,
-            kline_count=3,
+        klines_list = self.create_klines(
             symbol=self.exchange_info,
-            interval=self.interval_1m,
+            count=10,
         )
 
-        result = moving_average.get_value(kline=kline_older)
-        self.assertEqual(result, 25)
+        # moving_average = MovingAverage.objects.create(
+        #     name='some_name',
+        #     type=MovingAverage.Type.SMA,
+        #     kline_count=3,
+        #     symbol=self.exchange_info,
+        #     interval=self.interval_1m,
+        # )
+    #
+    #     result = moving_average.get_value(kline=kline_older)
+    #     self.assertEqual(result, 25)
