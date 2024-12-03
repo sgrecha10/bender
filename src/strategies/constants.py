18913fa194e437e2e69f295d82a7e0c99bc3a1c4
grechnev <sgrecha10@gmail.com>
2024-12-03 17:26:15 +0300

    some fix

--- src/strategies/constants.py
+++ src/strategies/constants.py
@@ -1,6 +1,5 @@
-from .models import Strategy
 from .backend.strategies import StrategyFirstBackend
-
+from .models import Strategy
 
 CODENAME_MAP = {
     Strategy.Codename.STRATEGY_1: StrategyFirstBackend,
