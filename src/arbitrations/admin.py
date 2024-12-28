69cfb75f7ca0b8cf050ad5d73293ea1e574ff77f
grechnev <sgrecha10@gmail.com>
2024-12-29 02:00:41 +0300

    some fix

--- src/arbitrations/admin.py
+++ src/arbitrations/admin.py
@@ -4,6 +4,7 @@ from .models import Arbitration
 
 @admin.register(Arbitration)
 class ArbitrationAdmin(admin.ModelAdmin):
+    change_form_template = 'admin/arbitrations/change_form.html'
     list_display = (
         'id',
         'codename',
@@ -18,3 +19,42 @@ class ArbitrationAdmin(admin.ModelAdmin):
         'symbol_2',
     )
     list_display_links = ('codename',)
+    fieldsets = [
+        ('Main', {
+            'fields': [
+                'codename',
+                'symbol_1',
+                'symbol_2',
+                'interval',
+                'start_time',
+                'end_time',
+
+            ],
+            'classes': ('grp-collapse', 'grp-open'),
+        }),
+        ('Difference', {
+            'fields': [
+                'price_comparison',
+                'moving_average',
+                'standard_deviation',
+
+            ],
+            'classes': ('grp-collapse', 'grp-open'),
+        }),
+        ('Conditions deal', {
+            'fields': [
+                'open_deal_sd',
+                'close_deal_sd',
+                'fixed_bet_amount',
+            ],
+            'classes': ('grp-collapse', 'grp-open'),
+        }),
+        ('Conditions test', {
+            'fields': [
+                'entry_price_order',
+                'maker_commission',
+                'taker_commission',
+            ],
+            'classes': ('grp-collapse', 'grp-open'),
+        }),
+    ]
--- src/arbitrations/models.py
+++ src/arbitrations/models.py
@@ -8,6 +8,14 @@ from market_data.constants import AllowedInterval
 
 class Arbitration(BaseModel):
 
+    class EntryPriceOrder(models.TextChoices):
+        OPEN = 'OPEN', 'Open price'
+        CLOSE = 'CLOSE', 'Close price'
+        HIGH = 'HIGH', 'High price'
+        LOW = 'LOW', 'Low price'
+        MAXMIN = 'MAXMIN', 'MaxMin'
+        MINMAX = 'MINMAX', 'MinMax'
+
     class PriceComparison(models.TextChoices):
         OPEN = 'open_price', 'Open price'
         CLOSE = 'close_price', 'Close price'
@@ -15,53 +23,91 @@ class Arbitration(BaseModel):
         LOW = 'low_price', 'Low price'
 
     codename = models.CharField(
-        verbose_name='Codename',
         max_length=100,
+        verbose_name='Codename',
     )
     symbol_1 = models.ForeignKey(
         ExchangeInfo,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Symbol 1',
         related_name='arbitration_set_first',
+        verbose_name='Symbol 1',
     )
     symbol_2 = models.ForeignKey(
         ExchangeInfo,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name='Symbol 2',
         related_name='arbitration_set_second',
+        verbose_name='Symbol 2',
     )
     interval = models.CharField(
-        verbose_name='Base interval',
         choices=AllowedInterval.choices,
         default=AllowedInterval.MINUTE_1,
         max_length=10,
+        verbose_name='Base interval',
     )
     start_time = models.DateTimeField(
-        verbose_name='Start time',
         null=True, blank=True,
+        verbose_name='Start time',
     )
     end_time = models.DateTimeField(
-        verbose_name='End time',
         null=True, blank=True,
+        verbose_name='End time',
     )
     price_comparison = models.CharField(
-        verbose_name='Price comparison',
         choices=PriceComparison.choices,
         default=PriceComparison.CLOSE,
+        verbose_name='Price comparison',
     )
-    moving_average = models.ForeignKey(  # или стразу индикатор стандартного отклонения сюда?
+    moving_average = models.ForeignKey(
         MovingAverage,
         on_delete=models.SET_NULL,
-        null=True,
+        null=True, blank=True,
         verbose_name='Moving average',
     )
     standard_deviation = models.ForeignKey(
         StandardDeviation,
         on_delete=models.SET_NULL,
-        null=True,
+        null=True, blank=True,
         verbose_name='Standard deviation',
+    )
+    open_deal_sd = models.DecimalField(
+        max_digits=20,
+        decimal_places=10,
+        null=True, blank=True,
+        verbose_name='Open deal SD',
+    )
+    close_deal_sd = models.DecimalField(
+        max_digits=20,
+        decimal_places=10,
+        null=True, blank=True,
+        verbose_name='Close deal SD',
+    )
+    fixed_bet_amount = models.DecimalField(
+        max_digits=20,
+        decimal_places=10,
+        default=1.0000,
+        verbose_name='Fixed bet amount',
+        help_text='Сумма двух инструментов в сделке',
+    )
+    entry_price_order = models.CharField(
+        max_length=50,
+        choices=EntryPriceOrder.choices,
+        default=EntryPriceOrder.MAXMIN,
+        verbose_name='Entry price order',
+        help_text='В каком порядке подавать цены свечи при тестировании стратегии',
+    )
+    maker_commission = models.DecimalField(
+        max_digits=4,
+        decimal_places=3,
+        default=0,
+        verbose_name='Maker commission',
+    )
+    taker_commission = models.DecimalField(
+        max_digits=4,
+        decimal_places=3,
+        default=0,
+        verbose_name='Taker commission',
     )
 
     class Meta:
