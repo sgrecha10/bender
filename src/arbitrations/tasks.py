d850db645d6d2f309b6ce6e6a36131ab44bcc973
grechnev <sgrecha10@gmail.com>
2025-01-06 16:09:51 +0300

    some fix

--- src/arbitrations/tasks.py
+++ src/arbitrations/tasks.py
@@ -30,12 +30,37 @@ def run_arbitration_test_mode(self, arbitration_id: int):
     for kline_1, kline_2 in zip(kline_qs_1, kline_qs_2):
         open_time_1 = kline_1.open_time
         open_time_2 = kline_2.open_time
-
         if open_time_1 != open_time_2:
             raise ValueError('Incorrect opening times')
+        deal_time = open_time_1
 
-        # цены, между которыми надо определять кросс курс. при тестировании Entry price order
-        price_1 = kline_1.open_price
-        price_2 = kline_2.open_price
+        # выбираем порядок подачи тестовых цен на вход в стратегию
+        entry_price_order_map = {
+            Arbitration.EntryPriceOrder.MAXMIN: (
+                (kline_1.high_price, kline_2.high_price),
+                (kline_1.low_price, kline_2.low_price),
+            ),
+            Arbitration.EntryPriceOrder.MINMAX: (
+                (kline_1.low_price, kline_2.low_price),
+                (kline_1.high_price, kline_2.high_price),
+            ),
+            Arbitration.EntryPriceOrder.OPEN: (
+                (kline_1.open_price, kline_2.open_price),
+            ),
+            Arbitration.EntryPriceOrder.CLOSE: (
+                (kline_1.close_price, kline_2.close_price),
+            ),
+            Arbitration.EntryPriceOrder.HIGH: (
+                (kline_1.high_price, kline_2.high_price),
+            ),
+            Arbitration.EntryPriceOrder.LOW: (
+                (kline_1.low_price, kline_2.low_price),
+            ),
+        }
 
-        backend.run_step(price_1=price_1, price_2=price_2, deal_time=open_time_1)
+        for price_1, price_2 in entry_price_order_map.get(arbitration.entry_price_order, []):
+            backend.run_step(
+                price_1=price_1,
+                price_2=price_2,
+                deal_time=deal_time,
+            )
